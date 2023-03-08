d52168288bc5c29d377575f63371fba7b3f165c1
finlayclark <s1629668@ed.ac.uk>
2023-03-08 11:57:25 +0000

    Improve overall analysis script
    This now analyses all repeats for all ligands in the directory,
    reports (and is tolerant of) failures, converts output to a pandas
    dataframe,and writes to a csv.

--- repex_overall_analysis.py
+++ repex_overall_analysis.py
@@ -1,21 +1,17 @@
+import glob
+from unittest import skip
 from repex_analysis import get_correction
 from openmmtools.multistate import MultiStateSamplerAnalyzer, MultiStateReporter
-from openmmtools.openmm_torch.repex import get_atoms_from_resname
-from openmm import app
 import numpy as np
 import os
+import pandas as pd
 import argparse
-from rdkit import Chem
-from rdkit.Chem import Draw
-import matplotlib.pyplot as plt
-import seaborn as sns
-from MDAnalysis.lib.distances import calc_dihedrals
-import MDAnalysis as mda
-import mdtraj
 import logging
 
 logger = logging.getLogger(__name__) 
+logger.setLevel(logging.INFO)
 
+# Not used in current version of script
 def get_overall_correction(analysers_dict, temp):
     """Get overall correction - as calculated over all supplied 
     calculations.
@@ -50,6 +46,7 @@ def get_overall_correction(analysers_dict, temp):
     return overall_results_dict
 
 
+# Not used in current version of script
 def write_overall_correction(analysers_dict, temp, out_file):
     """Write overall correction - as calculated over all supplied 
     calculations - to file.
@@ -71,50 +68,108 @@ def write_overall_correction(analysers_dict, temp, out_file):
             f"Solvent: {corr_dict['solvent']['dg']:.3f} +/- {corr_dict['solvent']['er']:.3f} kcal/mol\n")
 
 
-def analyse_all(repeat_dirs, lig_names, temp, outname):
-    """Perform joint analysis of results for all replicate runs in "repeat_dirs".
+def get_overall_results_dict(output_dirs, temp=300):
+    """Perform joint analysis of results for all replicate runs in "repeat_dirs". Assumes
+    that the directories are named {lig_name}_{rep_num}, where rep_num is < 10. Writes output
+    to a CSV file.
 
     Args:
-        repeat_dirs (list): List of paths to directories containing replicate runs.
-        lig_names (list): List of ligand names for which to perform analysis.
-        temp (int): Temperature in K.
-        outname (str): Name of output directory.
+        output_dirs (list): List of paths to directories containing the outputs for each replicate run.
+        temp (int, optional): Temperature in K. Defaults to 300 K.
+
+    Returns:
+        results (dict): Dictionary of results for each ligand. Has the form {lig_name: {rep_num: {"dg": dg, "er": er}, ...}, ...}.
     """
+    # Create dictionary of ligand names and replicate numbers to store results
+    results = {}
+    for d in output_dirs:
+        lig_name = d[:-2]
+        rep_num = int(d[-1])
+        if lig_name not in results:
+            results[lig_name] = {}
+        results[lig_name][rep_num] = {}
 
-    for lig in lig_names:
-
-        # Dictionary to store all the analysers
-        analysers_dict = {}
+    # Get the results for each ligand
+    for lig_name in results:
+        logger.info(f"Analysing ligand {lig_name}")
 
         # Populate dict for each replicate
-        for rep in repeat_dirs:
+        for rep_no in results[lig_name]:
+            dir_name = f"{lig_name}_{rep_no}"
+
+            # Check if the simulation crashed - if so, skip
+            skip_analysis = False
+            for phase in ["complex", "solvent"]:
+                if os.path.exists(os.path.join(dir_name, lig_name, phase, "nan-error-logs")):
+                    logger.error(f"Simulation for {lig_name} in {phase} phase crashed for replicate {rep_no}. Skipping analysis.")
+                    results[lig_name][rep_no] = {"dg": np.nan, "er": np.nan}
+                    skip_analysis = True
+            if skip_analysis:
+                continue
+
             # Get path to complex and solvent nc files
-            com_nc_file = os.path.join(rep, lig, "complex", "repex.nc")
-            sol_nc_file = os.path.join(rep, lig, "solvent", "repex.nc")
+            # TODO: Fix redundant directory structure with ligand name subdir
+            com_nc_file = os.path.join(dir_name, lig_name, "complex", "repex.nc")
+            sol_nc_file = os.path.join(dir_name, lig_name, "solvent", "repex.nc")
+
             # Get analysers
             rep_com = MultiStateReporter(com_nc_file)
             rep_sol = MultiStateReporter(sol_nc_file)
             ana_com = MultiStateSamplerAnalyzer(rep_com)
             ana_sol = MultiStateSamplerAnalyzer(rep_sol)
-            # Add to dict
-            analysers_dict[rep] = {"ana_com": ana_com, "ana_sol": ana_sol}
 
-            # Get the overall correction
-            if not os.path.exists(outname):
-                os.makedirs(outname)
-            write_overall_correction(analysers_dict, temp, os.path.join(outname, f"{lig}_overall_correction.txt"))
+            # Get correction for a single replicate
+            cor_dict = get_correction(ana_com, ana_sol, temp)
+            dg = cor_dict["corr"]["dg"]
+            er = cor_dict["corr"]["er"]
+            results[lig_name][rep_no] = {"dg": dg, "er": er}
+
+        # Get the overall stats, including inter- and intra-replicate error in the overall,
+        # and ignoring any nans
+        mean_corr = np.nanmean([results[lig_name][rep_no]["dg"] for rep_no in results[lig_name]])
+        er_inter = np.nanstd([results[lig_name][rep_no]["dg"] for rep_no in results[lig_name]])
+        er_intra = np.sqrt(np.nansum([results[lig_name][rep_no]["er"]**2 for rep_no in results[lig_name]]))
+        er_overall = np.sqrt(er_inter**2 + er_intra**2)
+        results[lig_name]["overall"] = {"dg": mean_corr, "er": er_overall}
+
+    # Return the overall results
+    return results
+
+def format_dict_to_pandas(results_dict):
+    """Format results dictionary into a pandas dataframe.
+
+    Args:
+        results_dict (dict): Dictionary of results for each ligand. 
+        Has the form {lig_name: {rep_num: {"dg": dg, "er": er}, ...}, ...}.
+
+    Returns:
+        res_df (pandas.DataFrame): Pandas dataframe of results.
+    """
+    # Create a list of dicts, where each dict is a row in the dataframe
+    rows = []
+    for lig_name in results_dict:
+        for repeat in results_dict[lig_name]:
+            row = {"lig_name": lig_name, "repeat": repeat}
+            row.update(results_dict[lig_name][repeat])
+            rows.append(row)
+
+    # Create the dataframe
+    res_df = pd.DataFrame(rows)
+    res_df.set_index(["lig_name", "repeat"], inplace=True)
+    res_df.rename(columns={"dg": "Delta G (kcal / mol)", "er": "Error (kcal / mol)"}, inplace=True)
+    
+    return res_df
+
 
 def main():
     parser = argparse.ArgumentParser(
        description="Analyse results from multiple replicate runs.")
     parser.add_argument(
-       "--repeat_dirs",
-       nargs="+",
-       help="Paths to directories containing replicate runs.")
-    parser.add_argument(
-       "--lig_names",
-       nargs="+",
-       help="Names of ligands for which to perform analysis.")
+       "--overall_outdir",
+       type = str,
+       default=".",
+       help="Path to top-level directory containing the output directories for " 
+       "each replicate run for each ligand. Defaults to current directory.")
     parser.add_argument(
        "--temp",
        type=int,
@@ -122,11 +177,23 @@ def main():
        help="Temperature in K. Defaults to 300.")
     parser.add_argument(
        "--outname",
-       help="Name of output directory.")
+       type=str,
+       default="overall_results",
+       help="Name of output file.")
     arg = parser.parse_args()
 
-    analyse_all(arg.repeat_dirs, arg.lig_names, arg.temp, arg.outname)
+    # Get input files - assumes naming convention {lig_name}_{rep_num}
+    out_dirs = [f for f in glob.glob("*_?") if "__" not in f] # Exclude __pycache__ 
 
+    # Analyse and write output
+    res_dict = get_overall_results_dict(out_dirs, arg.temp)
+    res_df = format_dict_to_pandas(res_dict)
+    res_df.to_csv(f"{arg.outname}.csv")
+
+    # Print out which ligands have failed
+    failed_ligs = [lig for lig in res_df.index if np.isnan(res_df.loc[lig]["Delta G (kcal / mol)"])]
+    for lig in failed_ligs:
+        logger.error(f"Calculation for ligand {lig[0]}, repeat {lig[1]} failed.")
 
 if __name__ == "__main__":
     main()
